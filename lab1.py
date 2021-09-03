cc56e3d553a18d6ed21bf2c556e24ba2a4d3ee67
Maristella Jho <56034486+hyunseo6579@users.noreply.github.com>
2021-09-03 15:03:38 -0600

    final

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
 import requests
 
-r=requests.get('https://github.com/hyunseo6579/404_lab1/blob/main/lab1.py')
+r=requests.get('https://raw.githubusercontent.com/hyunseo6579/404_lab1/master/lab1.py')
 print(r.text)
